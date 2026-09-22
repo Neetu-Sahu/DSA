e45b50c1f1b854cbebcf4e94513a859f3090c92d
NEETU SAHU <neetusahu183@gmail.com>
2026-09-22 16:52:58 +0530

    Time: 3 ms (57.99%) | Memory: 13.2 MB (94.39%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,11 +1,18 @@
 class Solution(object):
+
+    #O(nlog n) complexity
     def missingNumber(self, nums):
-        
-        nums.sort()
-        for a,b in enumerate(nums):
-            if a!=b:
-                return a
-        if len(nums) not in nums:
-            return len(nums)
+    #   
+    #  nums.sort()
+    # for a,b in enumerate(nums):
+    #    if a!=b:
+    #       return a
+    #if len(nums) not in nums:
+    #   return len(nums)
+
+    #O(n) time complexity
+         return sum(range(len(nums)+1))-sum(nums)
+
+
         
         
